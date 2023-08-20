a74910b5df1d0f9d4b8b7f3d33ea5db71d15bd98
Cwait00 <cwaitanobongoza@gmail.com>
2023-08-20 16:40:51 -0700

    repo_printf.c

---
+++ repo_printf.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stddef.h>
+
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - function of a printf
+ * @format: format
+ * Return: The number of characters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	int x, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+	va_start(list, format);
+
+	for (x = 0; format && format[x] != '\0'; x++)
+	{
+		if (format[x] != '%')
+		{
+			buffer[buff_ind++] = format[x];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[x], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &x);
+			width = get_width(format, &x, list);
+			precision = get_precision(format, &x, list);
+			size = get_size(format, &x);
+			++x;
+					mat, &x, list;
+			size = get_size(format, &x);
+			++x;
+			printed = handle_print(format, &x, list, buffer,
+					flags, width, precision, size);
+
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+/**
+ * print_buffer - prints the contents of the buffer
+ * @buffer: Array of characters
+ * @buff_ind: index that adds the next character, represents the legnth.
+ */
+
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
