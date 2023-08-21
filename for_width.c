eaae5aa35c3a0b6826e2cb41fddf6c2255a2ece9
Lilita <lilitambete2911@gmail.com>
2023-08-21 15:50:14 -0700

    fixed handle_print.c

--- for_width.c
+++ for_width.c
@@ -1,27 +1,29 @@
 #include "main.h"
 /**
- * for_width - get width of format
- * @format: strings and number
- * @x: index holder
+ * for_width - print calculations of the width for printf
+ * @format: formatted strings of the printed arguments
+ * @x: list of printed arguments
+ * @list: list of arguments
  * Return: width
  */
-int for_width(const char *format, int *x)
+int for_width(const char *format, int *x, va_list list)
 {
-	int print_width;
+	int print_width = 0;
 	int a;
 
-	if (format[x] == '%')
+	if (format[*x] == '%')
 	{/*find and move away from %*/
-		x++;
+		*x++;
 	}
 
-	while (format[x] => 48 $$ format[x] <= 57)
+	/*"%9d"  "123456" "   123456"*/
+	while (format[*x] >= 48 && format[*x] <= 57)
 	{
 		/*to determind how many places need to be filled with spaces*/
-		if (format[x] < print_width)
+		if (format[*x] < print_width)
 		{
 			a = 0;
-			while (a < print_width - strlen(format[x]))
+			while (a < print_width - strlen(format[*x]))
 			{
 				_putchar(' ');
 				a++;
--- handle_print.c
+++ handle_print.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * handle_print - Prints a type of argument
  * @format: Formatted strings
@@ -12,41 +11,48 @@
  * @size: Size
  * Return: 1 or 2
  */
-
 int handle_print(const char *format, int *ind, va_list list, char buffer[],
 		int flags, int width, int precision, int size)
 {
-	int a, unknown_len = 0, printed_chars = -1;
+	int a = 0, unknown_len = 0, printed_chars = 0;
 	format_t format_types[] = {
 		('c', print_char), ('s', print_string), ('%', print_percent),
 		('i', print_int), ('d', print_int), ('b', print_binary),
-		('u', print_unsigned), ('o', print_octal), ('x', print_hexadecimal),
-		('x', print_hexa_upper), ('p', print_pointer), ('S', print_non_printable),
+		('u', print_unsigned_int), ('o', print_octal), ('x', print_hexadecimal),
+		('X', print_hexa_upper), ('p', print_pointer), ('S', print_non_printable),
 		('r', print_reverse), ('R', print_rot13string), ('\0', NULL)
 	};
 	for (a = 0; format_types[a].format != '\0'; a++)
-		if (format[*ind] == format_types[a].format)
-			return (format_types[a].fn(list, buffer, flags, width, precision, size));
-
-	if (format_types[a].format =='\0')
 	{
-		if ((format[*ind]) == '\0')
-			return (-1);
-		unknown_len += write(1, "%%", 1);
-		if (format[*ind - 1] == ' ')
-			unknown_len =+ write(1, " ", 1);
-		else if (width)
+		if (format[*ind] == format_types[a].format)
+		{
+			return (format_types[a].fn(list, buffer, flags, width, precision, size));
+		}
+	}
+	if (format[*ind] == '\0')
+	{/*if we are here, something is wrong*/
+		return (-1);
+	}
+
+	unknown_len += write(1, "%%", 1);
+
+	if (format[*ind - 1] == ' ')
+	{
+		unknown_len += write(1, " ", 1);
+	}
+	else if (width)
+	{
+		--(*ind);
+		while (format[*ind] != ' ' && format[*ind] != '%')
 		{
 			--(*ind);
-			while (format[*ind] != ' ' && format[*ind] != '%')
-				--(*ind);
-			if (format [*ind] == ' ')
-				--(*ind);
-
-			return (1);
 		}
-		unknown_len += write(1, &format[*ind], 1);
-		return (unknown_len);
+		if (format[*ind] == ' ')
+			--(*ind);
+		return (1);
 	}
-	return (printed_chars);
+	unknown_len += write(1, &format[*ind], 1);
+	return (unknown_len);
+}
+return (printed_chars);
 }
