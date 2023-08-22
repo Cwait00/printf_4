091bba3eda18ea5d00d70a5c593c53feb038da6b
Lilita <lilitambete2911@gmail.com>
2023-08-21 19:10:15 -0700

    unfinished file, completed.

--- for_width.c
+++ for_width.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * for_width - print calculations of the width for printf
  * @format: formatted strings of the printed arguments
@@ -13,20 +14,29 @@ int for_width(const char *format, int *x, va_list list)
 
 	if (format[*x] == '%')
 	{/*find and move away from %*/
-		*x++;
+		(*x)++;
 	}
 
 	/*"%9d"  "123456" "   123456"*/
-	while (format[*x] >= 48 && format[*x] <= 57)
-	{
-		/*to determind how many places need to be filled with spaces*/
-		if (format[*x] < print_width)
+	while (format[*x] >= '0' && format[*x] <= '9')
+	{/*making sure the characters are changed to integer by -'0'*/
+		print_width *= 10 + (format[*x] - '0');
+		(*x)++;
+	}
+	if (print_width > 0)
+	{/* to determine how many places need spaces*/
+		a = 0;
+		/*sizeof is used cause format[*x] is a single character*/
+		while (a < print_width - sizeof(char))
 		{
-			a = 0;
-			while (a < print_width - strlen(format[*x]))
-			{
-				_putchar(' ');
-				a++;
-			}
+			_putchar (' ');
+			a++;
 		}
-		/*this is where i lost my train of thought*/
+	}
+	else if (format[*x] == '*')
+	{/*check for * to then fetch the width from the argument instead */
+		print_width = va_arg(list, int);
+
+	}
+	return (print_width);
+}
