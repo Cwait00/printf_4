425a112456bf82fb9148b5d086d57d4fd9d6b6e3
Cwait00 <cwaitanobongoza@gmail.com>
2023-08-22 10:50:58 -0700

    get_flags-fixed

--- get_flags.c
+++ get_flags.c
@@ -14,8 +14,8 @@ int get_flags(const char *format, int *x)
 
 	int n, curr_x;
 	int flags = 0;
-	const char FLAGS_CH[] = ('-', '+', '0', '#', ' ', '\0');
-	const int FLAGS_ARR[] = (F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0);
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
 	for (curr_x = *x + 1; format[curr_x] != '\0'; curr_x++)
 	{
